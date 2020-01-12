b094f962b4780ffcd8b3dab5163b34f38473c83b
Tom Culpepper <tculpepp@gmail.com>
2020-01-12 13:14:21 -0600

    Initial commit. This is submitted version

---
+++ mod1/CSC212mod1Case/src/Greetings.java
@@ -0,0 +1,19 @@
+/**
+ * The Greetings implements an application
+ * that introduces me to the class.
+ * @author Tom Culpepper
+ *
+ */
+public class Greetings 
+{
+	 public static void main(String[] args)
+	 {
+		 //just hard code the information and print it!
+		 System.out.println("Greetings!");
+		 System.out.println("My name is Tom Culpepper");
+         System.out.print("I live in Destin, FL \r");
+         System.out.println("I am a Paramedic");
+         System.out.print("In my spare time I enjoy spending\rtime with my family and cycling.");
+	 }
+
+}
