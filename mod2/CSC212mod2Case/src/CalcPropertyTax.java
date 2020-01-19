f1f53a17158a9ab115d2f4b779f980e5142623eb
Tom Culpepper <tculpepp@gmail.com>
2020-01-18 19:50:18 -0600

    added pseudocode to CalcPropertyTax.Java and a few other comments in the code.  Also moved 3 variables to class instead of method

--- mod2/CSC212mod2Case/src/CalcPropertyTax.java
+++ mod2/CSC212mod2Case/src/CalcPropertyTax.java
@@ -3,27 +3,42 @@
  * that collects input from the user and uses it to 
  * calculate a property tax amount
  * @author Tom Culpepper
- *
+ * 
+ * Pseudocode:
+ * -import the scanner from util
+ * -set while loop in main to loop through userInteraction method
+ * -userInteraction method outputs strings and captures inputs using scanner.  (All inputs passed to validating methods then assigned to variables.)
+ *  requests ask for homeValue and taxRate then assign propertyTax to be homeValue * taxRate.
+ * -homeValue and taxRate are validated with validateUserInputFloat method to ensure only numbers are entered
+ * -print propertyTax
+ * -ask the user if they want to do another
+ * -validate the Y or N with validateRunAgain which returns it as a boolean
+ * -userInteraction does some interface housekeeping then returns the runAgain boolean to main
+ * -loop through again or say goodbye and exit depending on user boolean
  */
 import java.util.Scanner;
 
 public class CalcPropertyTax {
+	//initialize the class variables
+	static float houseValue;
+	static float taxRate;
+	static float propertyTax;
 
 	static Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-		boolean run;
+		boolean run; //boolean to either run again or exit the loop.  true=run again / false=exit
 		do {
 			run = userInteraction();	
 		}while (run);
 	}//end main method
 	
-	protected static boolean userInteraction() {
+	private static boolean userInteraction() {
 		System.out.print("Enter the deisred home value: $");
-		float houseValue = validateUserInputFloat(); //call the method to capture and validate the entry then assign it
+		houseValue = validateUserInputFloat(); //call the method to capture and validate the entry then assign it
 		System.out.print("Enter the current property tax rate percentage (%): "); 
-		float taxRate = (validateUserInputFloat()/100); //validate the input then convert the percentage to a decimal
-		float propertyTax = houseValue*taxRate;
+		taxRate = (validateUserInputFloat()/100); //validate the input then convert the percentage to a decimal
+		propertyTax = houseValue*taxRate;
 		System.out.println("Property tax due this year: $"+(String.format("%,.2f", propertyTax)));
 		System.out.print("\nWould you like to calculate another tax value? (y/n): ");	
 		boolean runAgain = (validateRunAgain(Character.toLowerCase((sc.next().charAt(0))))); //convert the input string to a single char and pass it to validation which will return a boolean
