d262ce68984be30a0ca9d8f0b93a0f0bffa4280b
Tom Culpepper <tculpepp@gmail.com>
2020-01-28 22:22:02 -0600

    Mod 2 SLP and Case simplified to meet assignment requirements strictly.

--- mod2/CSC212mod2Case/src/CalcPropertyTax.java
+++ mod2/CSC212mod2Case/src/CalcPropertyTax.java
@@ -6,13 +6,9 @@
  * 
  * Pseudocode:
  * -import scanner
- * -start userInteraction loop
  * -request and get hoseValue and taxRate (validate inputs)
- * -use getPropertytax method to get propertyTax (houseValue * taxRate)
+ * -calculate property tax
  * -print propertyTax
- * -ask user: run again or exit? (validate input)
- * -start over or end
- * 
  */
 import java.util.Scanner;
 
@@ -21,52 +17,23 @@ public class CalcPropertyTax {
 	static Scanner sc = new Scanner(System.in); //load the scanner
 	
 	public static void main(String[] args) {
-		boolean run; //boolean to either run again or exit the loop.  true=run again / false=exit
-		do {
-			run = userInteraction();	
-		}while (run);
-	}//end main method
-	
-	//a method to interact with the user.  All strings that are not data validation are here.
-	private static boolean userInteraction() {
-		System.out.print("Enter the deisred home value: $");
+		System.out.print("Enter the property value of the house or 0 to exit: $");
 		float houseValue = getUserInputFloat(); //call the method to capture and validate the entry then assign it
-		System.out.print("Enter the current property tax rate percentage (%): "); 
+		if (houseValue==0) {
+			System.exit(0);
+		}
+		System.out.print("Enter the property tax rate percentage (%): "); 
 		float taxRate = (getUserInputFloat()/100); //validate the input then convert the percentage to a decimal
-		float propertyTax = getPropertyTax(houseValue, taxRate);
+		float propertyTax = houseValue * taxRate; //calculate the property tax
 		System.out.println("Property tax due this year: $"+(String.format("%,.2f", propertyTax)));//format for readability ("%,.2f" inserts commas & 2 decimal places)
-		System.out.print("Would you like to calculate another tax value? (y/n): ");	
-		boolean runAgain = getRunAgain(); //get the user's input to go again or exit
-		System.out.print(runAgain ? "\n" : "*** Program Closed ***"); //either skip a line before starting over or let the user know we exited
-		return runAgain;//return the user's selection 
 	}
-	
+		
 	//a method to get and validate user input is a float
-	private static float getUserInputFloat() {
+	static float getUserInputFloat() {
 		while (!sc.hasNextFloat()) { //check to see if anything other than a float was entered
 			System.out.print("Please enter just a number: ");
 			sc.next();
 		}
 		return sc.nextFloat(); //return the validated number to the calling method
-	}
-	
-	//a method to get and validate the input and convert to a boolean
-	private static boolean getRunAgain() {
-		char runAgain = getScannerChar();
-		while (runAgain !='y' && runAgain !='n') { //check to see if anything other than y or n was entered
-			System.out.print("Please enter y or n: ");
-			runAgain = getScannerChar(); //update the variable and check again
-		}
-		return runAgain == 'y' ? true : false; //now that we are clean, return the appropriate boolean
-	}
-	
-	//a method to convert scanner string to a single lower case char
-	private static char getScannerChar() {
-		return Character.toLowerCase((sc.next().charAt(0)));
-	}
-	
-	//a method to calculate and return the property tax amount
-	private static float getPropertyTax(float houseValue, float taxRate) {
-		return houseValue * taxRate;
 	}
 }
--- mod2/CSC212mod2SLP/CSC212mod2SLP/src/IncomeTaxCalculator.java
+++ mod2/CSC212mod2SLP/CSC212mod2SLP/src/IncomeTaxCalculator.java
@@ -8,119 +8,48 @@
  * Pseudocode:
  * -import scanner
  * -initialize scanner
- * -initialize TaxBracket class array
- * -load TaxBracket objects into taxBrackets[]
- * -start userInteraction loop
  * -get taxable income (validate input)
- * -lookup tax rate in taxBrackets
- * -get tax amount from TaxBracket.getTaxAmount
- * -print tax rate and amount
- * -ask user: run again or exit? (validate input)
- * -start over or end
- * 
+ * -find correct tax rate
+ * -calculate tax amount
+ * -print tax amount 
  */
 import java.util.Scanner;
 
 public class IncomeTaxCalculator {
-
-	static Scanner sc = new Scanner(System.in); //load the scanner, we'll need it later
-	static TaxBracket taxBrackets[]= new TaxBracket[6];
 	
 	public static void main(String[] args) {
-
-		buildTaxBracketArray(); //load all the tax brackets here so we don't re-load for every run
-		boolean run; //boolean to either run again or exit the loop.  true=run again / false=exit
-		do {
-			run = userInteraction();	
-		}while (run);
-	}
-	//a method to interact with the user.  All strings that are not data validation are here.
-	private static boolean userInteraction() {
 		System.out.print("Enter your annual taxable income: $");
-		float taxableIncome = getUserInputFloat(); //call the method to capture and validate the entry then assign it
-		float taxRate = getTaxRate(taxableIncome); //pass the taxableIncome to getTaxRate to return the taxRate
-		float incomeTax = TaxBracket.getTaxAmount(taxableIncome, taxRate); //get incomeTax from TaxBracket
-		System.out.println("Your tax rate for this income is: "+(taxRate*100)+"%"); //we convert a decimal to a percent to make it more readable
-		System.out.println("Income tax due this year: $"+(String.format("%,.2f", incomeTax))); //format for readability ("%,.2f" inserts commas & 2 decimal places)
-		System.out.print("Would you like to calculate another tax value? (y/n): ");	
-		boolean runAgain = getRunAgain(); //get the user's input to go again or exit
-		System.out.print(runAgain ? "\n" : "*** Program Closed ***"); //either skip a line before starting over or let the user know we exited
-		return runAgain;//return the user's selection 
-	}
-	
-	//a method to get and validate user input is a float
-	private static float getUserInputFloat() {
+		Scanner sc = new Scanner(System.in); //load the scanner,
 		while (!sc.hasNextFloat()) { //check to see if anything other than a float is in scanner
 			System.out.print("Please enter just a number: ");
 			sc.next();
 		}
-		return sc.nextFloat(); //return the validated number to the calling method
-	}
-	
-	//a method to get and validate the input and convert to a boolean
-	private static boolean getRunAgain() {
-		char runAgain = getScannerChar();
-		while (runAgain !='y' && runAgain !='n') { //check to see if anything other than y or n was entered
-			System.out.print("Please enter y or n: ");
-			runAgain = getScannerChar(); //update the variable and check again
+		float taxableIncome = sc.nextFloat(); //call the method to capture and validate the entry then assign it
+		sc.close();
+		float taxRate = 0f; //tax rate as a decimal
+		if (taxableIncome >= 0 && taxableIncome <= 8500) { //start stepping through tax brackets to find the rate
+			taxRate = 0.1f;
 		}
-		return runAgain == 'y' ? true : false; //now that we are clean, return the appropriate boolean
-	}
-	
-	//a method to convert scanner string to a single lower case char
-	private static char getScannerChar() {
-		return Character.toLowerCase((sc.next().charAt(0)));
-	}
-	
-	//a method to load all the needed TaxBracket objects into an array taxBrackets. See TaxBrackets class for details
-	private static TaxBracket[] buildTaxBracketArray() {
-		taxBrackets[0] = new TaxBracket(0, 8500, 0.10f); //0–$8,500: 10%
-		taxBrackets[1] = new TaxBracket(8500, 34500, 0.15f); //$8,500–$34,500: 15%
-		taxBrackets[2] = new TaxBracket(34500, 83600, 0.25f); //$34,500–$83,600: 25%
-		taxBrackets[3] = new TaxBracket(83600, 174400, 0.28f); //$83,600–$174,400: 28%
-		taxBrackets[4] = new TaxBracket(174400, 379150, 0.33f); //$174,400–$379,150: 33%
-		taxBrackets[5] = new TaxBracket(379150, 0.35f); //$379,150 and above: 35% 
-		return taxBrackets;
-	}
-	
-	//a method to find the applicable taxRate based on the income given. Returns 0.0 if no match found.
-	private static float getTaxRate(float taxableIncome) {
-		float rate=0.0f; //variable to hold our taxRate for return
-		for (TaxBracket tb : taxBrackets) {
-			if (taxableIncome >= tb.low
-					&& (taxableIncome <= tb.high|| tb.high==0)) {
-				rate = tb.rate;
-				break;
-			}
+		else if (taxableIncome >= 8500 && taxableIncome <= 34500) {
+			taxRate = 0.15f;
 		}
-		return rate;
-	}
-}
-/**
- * The TaxBracket class is use to create TaxBracket objects based on the needs 
- * of the assignment. 
- * constructor example: TaxBracket(1000, 12000, 0.10)
- * @author tculpepp
- * 
- */
-class TaxBracket {
-	int low = 0; //low limit of the tax bracket
-	int high = 0; //high limit of the tax bracket
-	float rate = 0.0f; //tax rate as a decimal
-
-	protected TaxBracket (int low, int high, float rate) {
-		this.low = low;
-		this.high = high;
-		this.rate = rate;
-	}
-	
-	protected TaxBracket (int low, float rate) {
-		this.low = low;
-		this.rate = rate;
-	}
-	
-	//a method to calculate the amount of taxes due
-	public static float getTaxAmount (float taxableIncome, float taxRate) {
-		return taxableIncome * taxRate;
+		else if (taxableIncome >= 34500 && taxableIncome <= 83600) {
+			taxRate = 0.25f;
+		}
+		else if (taxableIncome >= 83600 && taxableIncome <= 174400) {
+			taxRate = 0.28f;
+		}
+		else if (taxableIncome >= 174400 && taxableIncome <= 379150) {
+			taxRate = 0.33f;
+		}
+		else if (taxableIncome >= 379150) {
+			taxRate = 0.35f;
+		}
+		if (taxRate==0) {
+			System.out.println("Something has gone wrong, please try again");
+			System.exit(1);
+		}
+		float incomeTax = (taxableIncome * taxRate); //calculate the income tax
+		System.out.println("Income tax due this year: $"+(String.format("%,.2f", incomeTax))); //format for readability ("%,.2f" inserts commas & 2 decimal places)
 	}
 }
